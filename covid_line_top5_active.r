2648b8cb6b7c00863eeae6767ec8ec4c3d0b7da1
Abhinav <Abhinav@Abhinavs-MacBook-Pro.local>
2020-07-16 21:19:24 -0500

    Replacing with latest version of R script on my local machine from June 28, 2020.

--- covid_line_top5_active.r
+++ covid_line_top5_active.r
@@ -12,7 +12,8 @@ long <- wide %>%
 	gather(Date, Cases, -x1, -county) %>%
 	rename(County = county) %>%
 	mutate(Date = as.Date(str_remove(Date, "active_cases_"), format = "%m_%d")) %>%
-	mutate(Cases = as.integer(Cases))
+	mutate(Cases = as.integer(Cases)) %>%
+	mutate(County = reorder(County, desc(Cases), sum))
 
 top5 <- c("Harris", "Dallas", "Tarrant", "Bexar", "Travis")
 
@@ -22,9 +23,11 @@ line <- long %>% filter(County %in% top5) %>%
 	geom_point(alpha = 0.1, show.legend = TRUE) +
 	geom_vline(xintercept = as.numeric(as.Date("2020-05-01")), color = "gray") + 
 	annotate("text", x = as.Date("2020-05-01"), y = 6500, label = "Texas Re-opens » ", color = "gray", hjust = 1) +
+	geom_vline(xintercept = as.numeric(as.Date("2020-07-03")), color = "gray") +
+	annotate("text", x = as.Date("2020-07-03"), y = 25000, label = "Abbott Issues Statewide Mask Mandate » ", color = "gray", hjust = 1) +
 	labs(title="Estimated Active Coronavirus Cases in Texas' 5 Most Populous Counties", 
-		subtitle="April 7, 2020 — June 6, 2020", 
-		caption="Source: Texas Department of State Health Services, Sunday, June 7, 2020", 
+		subtitle="April 7, 2020 — July 8, 2020", 
+		caption=paste("Source: Texas Department of State Health Services, ", Sys.Date(), sep=""), 
 		x="", 
 		y="") + 
 	theme_solarized_2(light = FALSE)
